5976fe3c4410b4cb9fe38b7d8c12d5120c09ce26
drna3r <na3r1404@gmail.com>
2018-01-16 16:23:31 +0330

    Dynamic Theme! :boom:

---
+++ index.php
@@ -0,0 +1,43 @@
+<!-- <head> -->
+<?php include_once 'head.php'; ?>
+
+<!-- Main Header -->
+<?php include_once 'header.php'; ?>
+
+<!-- Left side column. contains the logo and sidebar -->
+<?php include_once 'main-sidebar.php'; ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Page Header
+            <small>Optional description</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+            <li class="active">Here</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+        <!--------------------------
+          | Your Page Content Here |
+          -------------------------->
+
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<!-- Main Footer -->
+<?php include_once 'footer.php';?>
+
+<!-- Control Sidebar -->
+<?php include_once 'control-sidebar.php'; ?>
+
+<!-- REQUIRED JS SCRIPTS -->
+<?php include_once 'foot.php'; ?>
