ff0e41302b12f1d8026d36d720545b7907483982
drna3r <na3r1404@gmail.com>
2018-01-17 20:57:52 +0330

    Remove Demo.js from footer
    Bad Settig For min-height of new-customer

--- foot.php
+++ foot.php
@@ -42,8 +42,6 @@ switch ($pagename) { case "new-customer.php": ?>
     <script src="plugins/iCheck/icheck.min.js"></script>
     <!-- FastClick -->
     <script src="bower_components/fastclick/lib/fastclick.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="dist/js/demo.js"></script>
     <!-- Page script -->
     <script>
         $(function () {
