973e96d58deaa0295cb32b5ee26a5f53f456b715
drna3r <na3r1404@gmail.com>
2018-01-22 17:47:24 +0330

    Remove  / Add / Change!
    Remove : Some Js code / color and date picker
    Add: Referral Customer Code inpute / with select tag condition
    Change: Translate English to persian , DataTable Buttons

--- foot.php
+++ foot.php
@@ -46,6 +46,8 @@ switch ($pagename) { case "new-customer.php": ?>
     <script>
         //Persian DatePicker Config
         $(document).ready(function() {
+
+            //Persian Date Picker Lunch For BirthDay Test Input
             $(".pdpicker").pDatepicker({
                 observer: true,
                 format: 'YYYY/MM/DD',
@@ -55,72 +57,20 @@ switch ($pagename) { case "new-customer.php": ?>
                 }
             });
 
-        });
-        $(function () {
-            //Initialize Select2 Elements
-            $('.select2').select2()
-
-            //Datemask dd/mm/yyyy
-            $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
-            //Datemask2 mm/dd/yyyy
-            $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
-            //Money Euro
-            $('[data-mask]').inputmask()
-
-            //Date range picker
-            $('#reservation').daterangepicker()
-            //Date range picker with time picker
-            $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A' })
-            //Date range as a button
-            $('#daterange-btn').daterangepicker(
-                {
-                    ranges   : {
-                        'Today'       : [moment(), moment()],
-                        'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                        'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-                        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                        'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-                        'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-                    },
-                    startDate: moment().subtract(29, 'days'),
-                    endDate  : moment()
-                },
-                function (start, end) {
-                    $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+            //Enbale Referral Customer Code If Select introduction: referral
+            $("#rcode").prop('disabled', true);
+            $("#rcode").val('');
+            $('#introduction').on('change', function() {
+                if($('option:selected', this).val() == 'referral'){
+                    $("#rcode").prop('disabled', false);
+                }else {
+                    $("#rcode").prop('disabled', true);
+                    $("#rcode").val('');
                 }
-            )
+            })
+        });
 
-            //Date picker
-            $('#datepicker').datepicker({
-                autoclose: true
-            })
 
-            //iCheck for checkbox and radio inputs
-            $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-                checkboxClass: 'icheckbox_minimal-blue',
-                radioClass   : 'iradio_minimal-blue'
-            })
-            //Red color scheme for iCheck
-            $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
-                checkboxClass: 'icheckbox_minimal-red',
-                radioClass   : 'iradio_minimal-red'
-            })
-            //Flat red color scheme for iCheck
-            $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
-                checkboxClass: 'icheckbox_flat-green',
-                radioClass   : 'iradio_flat-green'
-            })
-
-            //Colorpicker
-            $('.my-colorpicker1').colorpicker()
-            //color picker with addon
-            $('.my-colorpicker2').colorpicker()
-
-            //Timepicker
-            $('.timepicker').timepicker({
-                showInputs: false
-            })
-        })
     </script>
     <?php break; case "customer-list.php": ?>
     <!---------------------------------------- customer-list.php ----------------------------------------------------------------------->
@@ -131,23 +81,28 @@ switch ($pagename) { case "new-customer.php": ?>
     <script src="bower_components/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
     <script src="bower_components/datatables.net-buttons/js/buttons.print.min.js"></script>
     <script src="bower_components/datatables.net-buttons/js/buttons.html5.min.js"></script>
+    <script src="bower_components/datatables.net-buttons/js/buttons.colVis.min.js"></script>
     <script src="bower_components/jszip/dist/jszip.min.js"></script>
     <!-- page script -->
     <script>
         $(function () {
             $('#example1').DataTable({
+                "stateSave" : true,
                 "bLengthChange": false,
                 "bInfo": false,
                 "oLanguage": {
                     "sSearch": "جستجوی مشتریان ",
                     "oPaginate": {
-                        "sNext": "صفحه یعد",
+                        "sNext": "صفحه بعد",
                         "sPrevious": "صفحه قبل"
                     },
                 },
                 dom: 'Bfrtip',
                 buttons: [
-                    'copy', 'excel', 'print'
+                    { extend: 'print', text: 'پرینت' },
+                    { extend: 'print', text: 'کپی' },
+                    { extend: 'excel', text: 'خروجی اکسل' },
+                    { extend: 'colvis', text: 'انتخاب ستون ها', columns: ':gt(1)'}
                 ]
             })
         })
