95294671dcd5de0660c81c9687c5cc37cab3ea89
drna3r <na3r1404@gmail.com>
2018-02-02 14:34:17 +0330

    Edit Sale Report Menu in Main Sidebar

--- main-sidebar.php
+++ main-sidebar.php
@@ -17,7 +17,19 @@
             <!-- Optionally, you can add icons to the links -->
             <li><a href="./new-customer.php"><i class="fa fa-id-card"></i> <span>مشتری جدید</span></a></li>
             <li><a href="./customer-list.php"><i class="fa fa-user-circle "></i> <span>لیست مشتریان</span></a></li>
-            <li><a href="./admin-panel.php"><i class="fa fa-bar-chart"></i> <span>گزارش فروش</span></a></li>
+            <li class="treeview menu-open">
+                <a href="#">
+                    <i class="fa fa-bar-chart"></i>
+                    <span>گزارش فروش</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu" style="display: block;">
+                    <li><a href="./sale-report-service.php"><i class="fa fa-circle-o"></i> بر اساس خدمت</a></li>
+                    <li><a href="./sale-report-invoice.php"><i class="fa fa-circle-o"></i> بر اساس فاکتور</a></li>
+                </ul>
+            </li>
             <li><a href="./admin-panel.php"><i class="fa fa-calendar-check-o"></i> <span>رزرواسیون</span></a></li>
             <li><a href="./admin-panel.php"><i class="fa fa-commenting"></i> <span>پیگیری</span></a></li>
             <li><a href="./admin-panel.php"><i class="fa fa-cogs"></i> <span>کنترل پنل</span></a></li>
