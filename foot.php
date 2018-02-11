31e931d5491cd1c55e4a99dd91dfda97433de296
drna3r <na3r1404@gmail.com>
2018-02-11 20:54:17 +0330

    Include Cleave.js Library

--- foot.php
+++ foot.php
@@ -9,6 +9,8 @@
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
+<!-- Cleave.js -->
+<script src="bower_components/cleave.js/dist/cleave.js"></script>
 
 
 <!-------------------------------
