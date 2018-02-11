a0b0a012ae8e1dfd13d4ea4b9c92c289e628d998
drna3r <na3r1404@gmail.com>
2018-02-11 17:41:58 +0330

    Remove_services With Ajax

--- ajax-db.php
+++ ajax-db.php
@@ -69,6 +69,33 @@ VALUES ('$name', '$price', '$credit_in_first_use', '$period_use', '$pu_t')";
         $conn->close();
         break;
 
+    /*--------------------------------------------- Form : remove_service ---------------------------------------------*/
+    case "remove_service":
+
+        $s_id = $_GET["s_id"];
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        //Set Charset UTF-8
+        $conn->set_charset("utf8");
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "DELETE FROM services_list WHERE id = $s_id";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "خدمت مورد نظر حذف شد!";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+        break;
+
+
     /*--------------------------------------------- Form : addpartner ---------------------------------------------*/
     case "addpartner":
 
